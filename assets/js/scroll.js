4c67af8b8395b8f764d36f8ba3aa9b76a6c75b85
mrcerverus_white <mr.arielvc@gmail.com>
2024-02-17 10:54:51 -0300

    Primer commit

---
+++ assets/js/scroll.js
@@ -0,0 +1,13 @@
+// Smooth Scroll
+
+let buttons = document.querySelectorAll('a');
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        let target = e.currentTarget.getAttribute('href');
+
+        document.querySelector(target).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
